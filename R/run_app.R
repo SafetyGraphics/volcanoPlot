b07236b5bbfb3396534447c7bc95b6079d085777
Becca Krouse <becca.krouse@gmail.com>
2022-02-18 14:30:46 -0500

    get working example

--- R/run_app.R
+++ R/run_app.R
@@ -31,37 +31,5 @@ run_app <- function(onStart = NULL, options = list(), enableBookmarking = NULL) 
     options = options,
     enableBookmarking = enableBookmarking
   )
-  #runApp(app, launch.browser = TRUE)
-  runApp(app, launch.browser = FALSE)
+  runApp(app, launch.browser = TRUE)
 }
-
-
-#runApp(app=shinyApp(ui=app_ui, server=app_server))
-
-
-######################## testing ###########################
-library(shiny) # version: 1.4.0
-library(shinyjs) # version: 1.1
-library(data.table) # version: 1.12.8
-library(DT) # version: 0.12
-library(ggplot2) # version: 3.2.1
-library(plotly) # version: 4.9.1
-library(scales) # version: 1.1.0
-library(shinycssloaders) # version: 0.3
-library(conflicted) # version: 1.0.4
-library(dplyr) # version: 0.8.3
-library(tidyr) # version: 1.0.2
-library(survival) # version: 3.1-8
-library(survminer) # version: 0.4.6
-library(fmsb) # version: 0.7.0
-library(shinyWidgets)
-library(cowplot)
-setwd("~/Desktop/volcano-plot/R")
-source("app_server.R")
-source("app_ui.R")
-source("GetStatistics.R")
-#source("~/Desktop/volcano-plot/archives/R/GetStatistics.R")
-source("util.R")
-source("volcano_plot.R")
-runApp(app=shinyApp(ui=app_ui, server=app_server))
-run_app()
