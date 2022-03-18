8d55e1bb5eb14d3329e99dfd722cd7ddc58e3ea2
Anh N Tran <trannhatanh89@gmail.com>
2022-03-18 00:11:44 -0500

    prelim volcano plot using EnhancedVolcano

--- R/volcanoPlot.R
+++ R/volcanoPlot.R
@@ -4,15 +4,15 @@
 #' @param plotly wrap output in ggplotly? default=TRUE
 #'  
 #' @return a volcano plot created with ggplot or plotly
-#'  
+#' 
 #' @examples
 #' settings<-list(
 #'   stratification_col="AEBODSYS",
-#'   group_col="ARM", 
+#'   group_col="ARM",
 #'   reference_group="Placebo",
 #'   comparison_group="Xanomeline High Dose",
 #'   id_col="USUBJID"
-#' ) 
+#' )
 #' stats<-getStats(dfAE=safetyData::adam_adae, dfDemog = safetyData::adam_adsl, settings)
 #' volcanoPlot(stats)
 #' 
@@ -21,20 +21,29 @@
 #' 
 #' @export
 
-volcanoPlot <- function(data, plotly=TRUE){
-  
-  p <- ggplot(data, aes(estimate, pvalue)) + 
-    geom_point(aes(size=eventN_total), pch=21, alpha=0.5, fill="skyblue2") + 
-    geom_hline(yintercept = 0.05, color = 'grey30', linetype = "dashed") +
-    geom_vline(xintercept = 1, color = 'grey30', linetype = "dashed") +
-    theme_classic() +
-    theme(legend.position = "none")+
-    scale_x_continuous("Comparison Group vs. Reference Group",expand = expansion(mult = c(0.05, 0.05)))+
-    scale_size_continuous(range = c(2, 12))
-  
-    if(plotly){
-      return(ggplotly(p))  
-    }else{
-      return(p)
-    }
-}  
+# volcanoPlot <- function(data, plotly=TRUE){
+# 
+#   p <- ggplot(data, aes(estimate, pvalue)) +
+#     geom_point(aes(size=eventN_total), pch=21, alpha=0.5, fill="skyblue2") +
+#     geom_hline(yintercept = 0.05, color = 'grey30', linetype = "dashed") +
+#     geom_vline(xintercept = 1, color = 'grey30', linetype = "dashed") +
+#     theme_classic() +
+#     theme(legend.position = "none")+
+#     scale_x_continuous("Comparison Group vs. Reference Group",expand = expansion(mult = c(0.05, 0.05)))+
+#     scale_size_continuous(range = c(2, 12))
+# 
+#     if(plotly){
+#       return(ggplotly(p))
+#     }else{
+#       return(p)
+#     }
+# }
+
+volcanoPlot <- function(data) {
+    p <- EnhancedVolcano(toptable = data,
+                         lab = 'strata',
+                         x = 'estimate',
+                         y = 'pvalue',
+                         pointSize = data$eventN_total)
+    return(p)
+}
