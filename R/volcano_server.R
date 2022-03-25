50125278248f11c930605f8dab2751c3310ad3e7
Jeremy Wildfire <jwildfire@gmail.com>
2022-03-25 12:00:53 -0400

    tweaks for sg compatability

--- R/volcano_server.R
+++ R/volcano_server.R
@@ -15,14 +15,14 @@
 volcano_server <- function(input, output, session, params) {
     ns <- session$ns
     print('starting server')
-    print(params)
     # create a custom mapping for stats calculation
     mapping<-reactive({
+        print(params()$settings)
         list(
             stratification_col=params()$settings$aes$bodsys_col,
-            group_col=params()$settings$dm$trt_col, 
-            reference_group=params()$settings$dm[['treatment_values--group1']],
-            comparison_group=params()$settings$dm[['treatment_values--group2']],
+            group_col=params()$settings$dm$treatment_col, 
+            reference_group=params()$settings$dm$treatment_values$group1,
+            comparison_group=params()$settings$dm$treatment_values$group2,
             id_col=params()$settings$dm$id_col
         )  
     })
