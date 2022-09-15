6fc80f23b77f9295f0934b88fef9d33213f769cb
Anh N Tran <trannhatanh89@gmail.com>
2022-09-15 13:57:13 -0500

    Update getStats so that it takes more than one comparison group

--- R/getStats.R
+++ R/getStats.R
@@ -10,11 +10,11 @@
 #' @examples
 #' settings<-list(
 #'   stratification_col="AEBODSYS",
-#'   group_col="ARM", 
+#'   group_col="ARM",
 #'   reference_group="Placebo",
-#'   comparison_group="Xanomeline High Dose",
+#'   comparison_group=c("Xanomeline High Dose","Xanomeline Low Dose"),
 #'   id_col="USUBJID"
-#' ) 
+#' )
 #' getStats(dfAE=safetyData::adam_adae, dfDemog = safetyData::adam_adsl, settings)
 #' 
 #' @import dplyr
@@ -27,70 +27,87 @@ getStats <- function(dfAE, dfDemog, settings, stat="Risk Ratio") {
     print(settings)
     dfDemog <- dfDemog %>% select(settings[["id_col"]], settings[["group_col"]])
     anly <- dfDemog %>% left_join(dfAE) # left join to keep all rows in dm (even if there were no AEs) 
-
-    N_comparison<-dfDemog %>% 
-    filter(.data[[settings$group_col]]==settings$comparison_group) %>% 
-    pull(.data[[settings$id_col]]) %>%
-    unique() %>%
-    length()
-
-    N_ref<-dfDemog %>% 
-    filter(.data[[settings$group_col]]==settings$reference_group) %>% 
-    pull(.data[[settings$id_col]]) %>%
-    unique() %>%
-    length()
-
-    aeCounts <- anly %>% 
-        filter(.data[[settings$group_col]] %in% c(settings$comparison_group, settings$reference_group))%>% 
-        group_by(.data[[settings$stratification_col]],.data[[settings$group_col]]) %>% 
+    aeCounts <- list()
+    
+    for (i in seq_along(settings$comparison_group)) {
+      
+      N_comparison <- dfDemog %>%
+        filter(.data[[settings$group_col]] == settings$comparison_group[i]) %>%
+        pull(.data[[settings$id_col]]) %>%
+        unique() %>%
+        length()
+      
+      N_ref <- dfDemog %>%
+        filter(.data[[settings$group_col]] == settings$reference_group) %>%
+        pull(.data[[settings$id_col]]) %>%
+        unique() %>%
+        length()
+      
+      aeCounts[[i]] <- anly %>%
+        filter(.data[[settings$group_col]] %in% c(settings$comparison_group[i], settings$reference_group)) %>%
+        group_by(.data[[settings$stratification_col]], .data[[settings$group_col]]) %>%
         # summarize(event=n())%>% do we need this too?
-        summarize(event = length(unique(.data[[settings$id_col]]))) %>% 
+        summarize(event = length(unique(.data[[settings$id_col]]))) %>%
         ungroup() %>%
-        na.omit %>% 
-        pivot_wider(names_from = .data[[settings$group_col]], values_from = "event", values_fill = 0) %>% 
+        na.omit %>%
+        pivot_wider(
+          names_from = .data[[settings$group_col]],
+          values_from = "event",
+          values_fill = 0
+        ) %>%
         rename(
-            strata=settings$stratification_col,
-            eventN_comparison=settings$comparison_group, 
-            eventN_ref=settings$reference_group
+          strata = settings$stratification_col,
+          eventN_comparison = settings$comparison_group[i],
+          eventN_ref = settings$reference_group
         ) %>%
         mutate(
-            eventN_total=eventN_comparison+eventN_ref,
-            N_comparison=N_comparison,
-            N_ref=N_ref,
-            N_total = N_comparison + N_ref
+          eventN_total = eventN_comparison + eventN_ref,
+          N_comparison = N_comparison,
+          N_ref = N_ref,
+          N_total = N_comparison + N_ref
         ) %>%
-        arrange(-1*eventN_total)
-
-    # calculate stats for each row
-    if(stat=="Risk Ratio"){
-        aeCounts <- aeCounts %>% 
-            rowwise %>% 
-            mutate(result = fmsb::riskratio(
-                            X=.data$eventN_comparison, 
-                            Y=.data$eventN_ref, 
-                            m1=.data$N_comparison, 
-                            m2=.data$N_ref) %>% list,
-                pvalue = result$`p.value`,
-                estimate = result$estimate) %>% 
-            ungroup %>% 
-            select(-result) %>% 
-            mutate(stat="Risk Ratio")
-    } else if (stat=="Risk Difference"){
-        aeCounts <- aeCounts %>% 
-            rowwise %>% 
-            mutate(result = fmsb::riskdifference(
-                a=.data$eventN_comparison, 
-                b=.data$eventN_ref, 
-                N1=.data$N_comparison, 
-                N0=.data$N_ref) %>% list,
-                pvalue = result$`p.value`,
-                estimate = result$estimate) %>% 
-            ungroup %>% 
-            select(-result) %>% 
-            mutate(stat="Risk Difference")
-    } else if(TRUE){
+        arrange(-1 * eventN_total)
+      
+      # calculate stats for each row
+      if (stat == "Risk Ratio") {
+        aeCounts[[i]] <- aeCounts[[i]] %>%
+          rowwise %>%
+          mutate(
+            result = fmsb::riskratio(
+              X = .data$eventN_comparison,
+              Y = .data$eventN_ref,
+              m1 = .data$N_comparison,
+              m2 = .data$N_ref
+            ) %>% list,
+            pvalue = result$`p.value`,
+            estimate = result$estimate,
+            comp_grp = settings$comparison_group[i]
+          ) %>%
+          ungroup %>%
+          select(-result) %>%
+          mutate(stat = "Risk Ratio")
+      } else if (stat == "Risk Difference") {
+        aeCounts[[i]] <- aeCounts[[i]] %>%
+          rowwise %>%
+          mutate(
+            result = fmsb::riskdifference(
+              a = .data$eventN_comparison,
+              b = .data$eventN_ref,
+              N1 = .data$N_comparison,
+              N0 = .data$N_ref
+            ) %>% list,
+            pvalue = result$`p.value`,
+            estimate = result$estimate,
+            comp_grp = settings$comparison_group[i]
+          ) %>%
+          ungroup %>%
+          select(-result) %>%
+          mutate(stat = "Risk Difference")
+      } else if (TRUE) {
         message("stat not supported yet :( ")
+      }
+      
     }
 
-    return(aeCounts)
+    return(bind_rows(aeCounts))
 }   
